2e07a816c6d169f741a93b757edd30da549ecbaa
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2025-08-13 16:22:51 -0300

    Exercicio3

---
+++ Desafio01-Rocketseat/OperacoesMatematicas.cs
@@ -0,0 +1,26 @@
+﻿namespace Desafio01_Rocketseat
+{
+    public static class OperacoesMatematicas
+    {
+        public static double Soma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public static double Subtracao(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static double Multiplicacao(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double Divisao(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public static double Media(double num1, double num2)
+        {
+            return (num1 + num2) / 2;
+        }
+    }
+}
--- Desafio01-Rocketseat/Program.cs
+++ Desafio01-Rocketseat/Program.cs
@@ -1,10 +1,34 @@
-﻿Console.Write("Digite seu primeiro nome: ");
-string primeiroNome = Console.ReadLine();
-Console.Write("Digite seu sobrenome: ");
-string sobrenome = Console.ReadLine();
+﻿using System.Globalization;
+using Desafio01_Rocketseat;
 
-string nomeCompleto = primeiroNome + " " + sobrenome;
+Console.WriteLine("Operações Matemáticas");
+Console.WriteLine("===============================");
+
+Console.Write("Digite o primeiro número: ");
+double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Digite o segundo número: ");
+double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
 Console.WriteLine("");
 
-Console.WriteLine($"Seu nome completo é: {nomeCompleto}");
+
+    double soma = OperacoesMatematicas.Soma(num1: n1, num2: n2);
+    double subtracao = OperacoesMatematicas.Subtracao(num1: n1, num2: n2);
+    double multiplicacao = OperacoesMatematicas.Multiplicacao(num1: n1, num2: n2);
+    double divisao = OperacoesMatematicas.Divisao(num1: n1, num2: n2);
+    double media = OperacoesMatematicas.Media(num1: n1, num2: n2);
+
+
+    Console.WriteLine($"A soma dos valores é: {soma}");
+    Console.WriteLine($"A subtração dos valores é: {subtracao}");
+    Console.WriteLine($"A multiplicação dos valores é: {multiplicacao}");
+    Console.WriteLine($"A divisão dos valores é: {divisao}");
+    Console.WriteLine($"A média dos valores é: {media}");
+
+
+
+
+
+
+
