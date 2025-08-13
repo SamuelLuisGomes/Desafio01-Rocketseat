3d31f4126d2b47e5c85e12a22774007eb6ed797a
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2025-08-13 15:54:36 -0300

    Exercicio2

--- Desafio01-Rocketseat/Program.cs
+++ Desafio01-Rocketseat/Program.cs
@@ -1,5 +1,10 @@
-﻿Console.Write("Digite seu nome: ");
+﻿Console.Write("Digite seu primeiro nome: ");
+string primeiroNome = Console.ReadLine();
+Console.Write("Digite seu sobrenome: ");
+string sobrenome = Console.ReadLine();
 
-string nome = Console.ReadLine();
+string nomeCompleto = primeiroNome + " " + sobrenome;
 
-Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+Console.WriteLine("");
+
+Console.WriteLine($"Seu nome completo é: {nomeCompleto}");
