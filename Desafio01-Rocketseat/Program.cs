311abe0e277d769d644791f7319ac12f0949f3da
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2025-08-13 13:15:08 -0300

    Exercicio1

--- Desafio01-Rocketseat/Program.cs
+++ Desafio01-Rocketseat/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite seu nome: ");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
