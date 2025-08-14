f447ed83725710446f9c5837bf3a32f1ff75732f
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2025-08-14 14:54:48 -0300

    Exercicio5

--- Desafio01-Rocketseat/Program.cs
+++ Desafio01-Rocketseat/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Desafio01_Rocketseat;
 
-Console.Write("Digite quantas palavras quiser: ");
-string frase = Console.ReadLine();
+Console.Write("Digite a placa de um veículo: ");
+string placa = Console.ReadLine();
+
+ValidacaoPlaca validaPlaca = new ValidacaoPlaca();
 Console.WriteLine("");
-Console.Write($"Quantidades de caracteres de todas as palavras digitadas: {ContagemCaracteres.ContadorCaractere(frase)}");
+Console.WriteLine($"a placa {placa} é {validaPlaca.Validacao(placa)}");
 
 
 
---
+++ Desafio01-Rocketseat/ValidacaoPlaca.cs
@@ -0,0 +1,40 @@
+﻿namespace Desafio01_Rocketseat
+{
+    public class ValidacaoPlaca
+    {
+        public string Validacao(string placa)
+        {
+            string placaFinal = LimpandoPlaca(placa);
+
+            if (!placaFinal.Contains("Falso"))
+            {
+                string letras = placaFinal.Substring(0, 3);
+                string numeros = placaFinal.Substring(3, 4);
+
+                if (letras.All(char.IsLetter) && int.TryParse(numeros, out _))
+                {
+                    return "Verdadeiro";
+                }
+                else
+                {
+                    return "Falso";
+                }
+            }
+            return "Falso";
+
+        }
+        public string LimpandoPlaca(string txt)
+        {
+            string placa = txt.Trim();
+            if (placa.Contains('-'))
+            {
+                placa = txt.Replace("-", "");
+            }
+            if (placa.Length < 7 || placa.Length > 7)
+            {
+                return "Falso";
+            }
+            return placa;
+        }
+    }
+}
