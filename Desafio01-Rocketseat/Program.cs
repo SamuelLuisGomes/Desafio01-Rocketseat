232965fcc47f384289d21d7b97b19bacb711be47
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2025-08-14 15:46:13 -0300

    Exercicio6

--- Desafio01-Rocketseat/Program.cs
+++ Desafio01-Rocketseat/Program.cs
@@ -1,11 +1,17 @@
-﻿using Desafio01_Rocketseat;
+﻿using System.Globalization;
+using Desafio01_Rocketseat;
 
-Console.Write("Digite a placa de um veículo: ");
-string placa = Console.ReadLine();
+Console.WriteLine("Data atual:");
+DateTime data = DateTime.UtcNow;
 
-ValidacaoPlaca validaPlaca = new ValidacaoPlaca();
-Console.WriteLine("");
-Console.WriteLine($"a placa {placa} é {validaPlaca.Validacao(placa)}");
+var dataLocal = data.ToLocalTime();
+CultureInfo culturaBrasileira = new CultureInfo("pt-BR");
+
+
+Console.WriteLine($"Formato completo: {culturaBrasileira.DateTimeFormat.GetDayName(dataLocal.DayOfWeek)},{dataLocal.Day}, {dataLocal.Month}, {dataLocal.Year}, {dataLocal.Hour},{dataLocal.Minute},{dataLocal.Second}");
+Console.WriteLine($"Apenas data: {dataLocal.ToString("dd/MM/yyyy")}");
+Console.WriteLine($"Apenas a hora: {dataLocal.Hour}:{dataLocal.Minute}");
+Console.WriteLine($"Data com mês por extenso: {data.ToString("dd/MMMM/yyyy")}");
 
 
 
