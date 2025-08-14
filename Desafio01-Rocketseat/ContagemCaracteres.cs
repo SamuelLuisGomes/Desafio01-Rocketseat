62e7ea3dccc76a3dc107f49fb5a2e302cef6627e
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2025-08-14 10:58:35 -0300

    Exercicio4

---
+++ Desafio01-Rocketseat/ContagemCaracteres.cs
@@ -0,0 +1,11 @@
+﻿namespace Desafio01_Rocketseat
+{
+    public static class ContagemCaracteres
+    {
+        public static int ContadorCaractere(string frase)
+        {
+            int qtdCaracteres = frase.Trim().Length;
+            return qtdCaracteres;
+        }
+    }
+}
--- Desafio01-Rocketseat/Program.cs
+++ Desafio01-Rocketseat/Program.cs
@@ -1,30 +1,10 @@
-﻿using System.Globalization;
-using Desafio01_Rocketseat;
+﻿using Desafio01_Rocketseat;
 
-Console.WriteLine("Operações Matemáticas");
-Console.WriteLine("===============================");
-
-Console.Write("Digite o primeiro número: ");
-double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-Console.Write("Digite o segundo número: ");
-double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
+Console.Write("Digite quantas palavras quiser: ");
+string frase = Console.ReadLine();
 Console.WriteLine("");
+Console.Write($"Quantidades de caracteres de todas as palavras digitadas: {ContagemCaracteres.ContadorCaractere(frase)}");
 
-
-    double soma = OperacoesMatematicas.Soma(num1: n1, num2: n2);
-    double subtracao = OperacoesMatematicas.Subtracao(num1: n1, num2: n2);
-    double multiplicacao = OperacoesMatematicas.Multiplicacao(num1: n1, num2: n2);
-    double divisao = OperacoesMatematicas.Divisao(num1: n1, num2: n2);
-    double media = OperacoesMatematicas.Media(num1: n1, num2: n2);
-
-
-    Console.WriteLine($"A soma dos valores é: {soma}");
-    Console.WriteLine($"A subtração dos valores é: {subtracao}");
-    Console.WriteLine($"A multiplicação dos valores é: {multiplicacao}");
-    Console.WriteLine($"A divisão dos valores é: {divisao}");
-    Console.WriteLine($"A média dos valores é: {media}");
 
 
 
